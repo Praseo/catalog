ffde8af864a2e346463ad94258c879f0d85e3afb
Danny Ferrin <danny.ferrin@praseo.ca>
2016-04-12 00:06:12 -0400

    adds catalog service slug routing

--- app/services/router.js
+++ app/services/router.js
@@ -8,12 +8,34 @@
       $stateProvider
       .state('services', {
          url: '/services',
-         controller: 'ServicesCtrl',
-         templateUrl: 'app/services/services.html',
          resolve: {
             catalog: ['servicesRepo', function (repo) {
                return repo.getCatalog();
             }]
+         },
+         views: {
+            '': {
+               template: '<div ui-view="main"></div>'
+            },
+            'main@services': {
+               controller: 'ServicesCtrl',
+               templateUrl: 'app/services/services.html',
+            }
+         },
+      })
+
+      .state('services.details', {
+         url: '/:slug',
+         resolve: {
+            catalogService: ['$stateParams', 'catalog', function ($stateParams, catalog) {
+               return _.find(catalog.services, {slug: $stateParams.slug});
+            }]
+         },
+         views: {
+            'main@services': {
+               controller: 'ServiceDetailsCtrl',
+               templateUrl: 'app/services/serviceDetails.html',
+            }
          }
       });
 
---
+++ app/services/serviceDetailsCtrl.js
@@ -0,0 +1,12 @@
+(function () {
+   'use strict';
+
+   angular
+   .module('catalog.services')
+   .controller('ServiceDetailsCtrl', ['$scope', 'catalog', 'catalogService', function ($scope, catalog, catalogService) {
+
+        $scope.catalog = catalog;
+        $scope.catalogService = catalogService;
+   }]);
+
+}());
